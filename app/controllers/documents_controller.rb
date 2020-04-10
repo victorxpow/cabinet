4d16a342decd10caac4fb13b41f3b14c48daa072
João Victor <victorpow@live.com>
2020-04-10 00:32:25 -0300

    show document sucess

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,4 +2,7 @@ class DocumentsController < ApplicationController
   def index
     @documents = Document.all
   end
+  def show
+    @document = Document.find(params[:id])
+  end
 end
--- spec/feature/store_document/user_view_documents_spec.rb
+++ spec/feature/store_document/user_view_documents_spec.rb
@@ -11,17 +11,20 @@ feature 'User view all documents' do
     expect(page).to have_content(document.id)
   end
 
-  xscenario 'User view document' do
-    login_as user, scope: :user
+  scenario 'User view document' do
+    document = create(:document)
+
     visit root_path
     click_on 'Documentos'
-    within("tr#document-#{document.id}") do
-      find("a[href='#{order_path(document)}'][data-method='get']").click
-    end
+    click_on document.title
 
-    expect(page).to have_content(document.user.id)
     expect(page).to have_content(document.title)
     expect(page).to have_content(document.body)
-    expect(page).to have_content(document.document_id)
+    expect(page).to have_content(document.id)
+  end
+  xscenario 'User view document' do
+    within("tr#document-#{document.id}") do
+    find("a[href='#{order_path(document)}'][data-method='get']").click
+    end
   end
 end
